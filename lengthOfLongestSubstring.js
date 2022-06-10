4113b247e7a426826a356b8e23d152d02fd4fba6
JasonYeon <jasonyeon110@gmail.com>
2022-06-09 22:48:28 -0700

    update

---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLongestSubstring = function(s) {
+    const map = {};
+    let left = 0;
+         
+      return s.split('').reduce((max, char, index) => {
+         
+          const lastSeenIndex = map[char];
+          const windowContainsCharacter = lastSeenIndex >= left;
+          left = windowContainsCharacter ? lastSeenIndex + 1 : left;
+          map[char] = index;
+          const windowSize = index - left + 1;
+          return Math.max(max, windowSize);
+          
+      }, 0);
+};
